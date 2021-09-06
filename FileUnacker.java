70fd7ccd61de7064a9e20464115d88e865d1a662
Nilam Mardane <44674622+mardanenilam@users.noreply.github.com>
2021-09-06 21:42:13 +0530

    Update FileUnacker.java

--- FileUnacker.java
+++ FileUnacker.java
@@ -15,46 +15,45 @@ import java.util.Arrays;
 
 public class FileUnacker
 {
+    //Characteristics
+    int headerSize=100; 
     FileOutputStream outstream = null; //
+    
     //If we want toexcute the code independetly, there should be main method
-    public static void main(String arr[]) throws Exception
-    {
+    public static void main(String arr[]) throws Exception    {
         FileUnacker obj = new FileUnacker("combine.txt"); // it will go to the constructor 
     }
     
-    public FileUnacker(String src) throws Exception //constructor ,here scr is combine.txt
-    {
+    public FileUnacker(String src) throws Exception{ //constructor ,here scr is combine.txt    
         unpack(src); //calling unpack function
     }
-    public void unpack(String filePath) //filepath is scr
-    {
-        try
-        {
-            FileInputStream instream = new FileInputStream(filePath); // to read file
-            byte header[]= new byte[100];  // array of header with 100 bytes
+    public void unpack(String filePath){ //filepath is scr
+    
+        try {
+            FileInputStream instream = new FileInputStream(filePath); // open the file to read
+            byte header[]= new byte[headerSize];  // array of header with 100 bytes
             int length = 0; 
             
-            while((length = instream.read(header,0,100)) > 0) // will interate till read complete 100 byte
-            {
+            while((length = instream.read(header,0,headerSize)) > 0){ // will interate till read all 100 byte
+            
                 String str = new String(header);  //converted 100 bytes into string, if we pass the array of bytes to the object string, it will convert automatically
                 
                 String ext = str.substring(str.lastIndexOf("/")); // need abc.txt which is after last "/" 
-                ext = ext.substring(1);  
+                ext = ext.substring(1);   
                 
-                String[] words=ext.split("\\s"); // it will seperate by using space // abc.txt and size
-                String filename = words[0]; // abc.txt  // array of string 
-                int size = Integer.parseInt(words[1]); // size of file// string need to convert into int, we are using wrapper class Integer
+                String[] words=ext.split("\\s"); // it will seperate by using space // split abc.txt and size
+                String filename = words[0]; // abc.txt  // array of string //abc.txt
+                int size = Integer.parseInt(words[1]); // size of file// e.g 10 bytes //string need to convert into int, we are using wrapper class Integer
                 
                 byte arr[] = new byte[size]; // array created to read data 
                 instream.read(arr,0,size); // header 
-                System.out.println(filename); //
+                //System.out.println(filename); 
                 FileOutputStream fout=new FileOutputStream(filename); //abc.txt file get created
                 fout.write(arr,0,size); // data, size written
             }
         }
         
-        catch(Exception e)
-        {
+        catch(Exception e){
             System.out.println(e);
         }
     }
